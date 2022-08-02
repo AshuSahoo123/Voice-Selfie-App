37494ef0c3fa279db06292fd639ae5c655f101f6
AshuSahoo123 <84637653+AshuSahoo123@users.noreply.github.com>
2022-08-02 13:37:27 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -12,17 +12,26 @@ recognition.onresult = function(event) {
     document.getElementById("textbox").innerHTML = content;
     console.log(content);
 
-    speak();
+    if (content == "take my selfie") {
+        speak();
+    }
 }
 
 function speak() {
     var synth = window.speechSynthesis;
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking your selfie in 5 seconds";
 
     var utter_this = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utter_this);
 
     Webcam.attach(camera);
+
+    setTimeout(
+        function() {
+        take_snapshot();
+        save();
+    }, 
+    5000);
 }
 
 Webcam.set({
@@ -33,3 +42,16 @@ Webcam.set({
 });
 
 camera = document.getElementById("camera");
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id = "selfie_image" src = "'+data_uri+'">';
+    });
+}
+
+function save() {
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
